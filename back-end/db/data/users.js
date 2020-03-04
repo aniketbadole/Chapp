9f348e80a5baa0c0252d3c3d04ae96246c5a4bfb
Kirsty McGlynn <kirsty.lmcg@gmail.com>
2020-03-04 15:39:06 +0000

    Database setup complete

---
+++ back-end/db/data/users.js
@@ -0,0 +1,11 @@
+module.exports = [
+  {
+    username: "Nick",
+    avatar_url:
+      "https://ya-webdesign.com/transparent250_/funny-png-avatar-2.png"
+  },
+  {
+    username: "Kirsty",
+    avatar_url: "https://avatarfiles.alphacoders.com/161/161216.png"
+  }
+];
